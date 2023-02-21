23777d7d3dadaf7e904cc356b948ae3b4065cf88
Tim18fak <Tim18fak@github.com>
2023-02-21 08:05:54 -0800

    task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-printf group project
+This project is a team project and it is about writing different functions in C
--- _printf.c
+++ _printf.c
@@ -1,45 +1,66 @@
-#include <unistd.h>
-#include "holberton.h"
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- *_printf - takes in a string and prints different types of arguments for
- * an unspecified amount of arguments
- * @format: the initial string that tell us what is going to be printed
- * Return: the amount of times we write to stdout
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, count;
+	int i, printed = 0, printed_chars = 0;
 
-	int (*f)(va_list);
+	int flags, width, precision, size, buff_ind = 0;
 
 	va_list list;
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(list, format);
-	i = count = 0;
 
-	while (format[i] != '\0')
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			f = get_func(format[i + 1]);
-			if (f == NULL)
-				count += print_nan(format[i], format[i + 1]);
-			else
-				count += f(list);
-			i++;
+			buffer[buff_ind++] = format[i];
+
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			count++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		i++;
 	}
+
+	print_buffer(buffer, &buff_ind);
 	va_end(list);
-	return (count);
+	return (printed_chars);
+}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
